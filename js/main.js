c789d2a7fab5f39871e18816bcbc905e7da334d8
marceloLinux <mmarcmartins@gmail.com>
2019-02-23 18:38:34 -0300

    Pagina inicial refeita

--- js/main.js
+++ js/main.js
@@ -157,6 +157,8 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const content = document.createElement('div');
+  content.className = "informations";
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
@@ -165,20 +167,23 @@ createRestaurantHTML = (restaurant) => {
   
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  content.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  content.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  content.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  content.append(more)
+
+
+  li.append(content);
 
   return li
 }
@@ -208,4 +213,38 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
+
+
+
+
+/* All Effects and Focus */
+
+
+ const menuButton = document.getElementById('menuButton');
+ const listMenu = document.getElementById('menuList');
+
+ menuButton.addEventListener('click', () => {
+    
+    if(!listMenu.classList.contains('active')){
+      listMenu.classList.add('active');
+      menuButton.classList.add('active');
+    }
+    else{
+      menuButton.classList.remove('active');
+      listMenu.classList.remove('active');
+    }
+
+
+ })
+
+
+
+
+
+
+
+
+
+
+
 
